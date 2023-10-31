c17e7e7d9af89ec616b4e10e76ae5b3273920e45
Yannis Marketakis <yannismarketakis@gmail.com>
2023-10-31 16:32:21 +0200

    updated SQL tables

--- StoryBoard/db/storyboard.sql
+++ StoryBoard/db/storyboard.sql
@@ -1,11 +1,11 @@
 CREATE TABLE user(
-	id varchar(36) NOT NULL, 
+	id int NOT NULL, 
 	username VARCHAR(128) NOT NULL,
 	PRIMARY KEY (id)
 );
 
 CREATE TABLE story(
-	id varchar(36) NOT NULL, 
+	id int NOT NULL, 
 	contents VARCHAR(256) NOT NULL,
 	post_date DATE NOT NULL,
 	by_user varchar(36) NOT NULL,
@@ -15,8 +15,8 @@ CREATE TABLE story(
 
 CREATE TABLE storylike(
 	like_date DATE NOT NULL,
-	by_user varchar(36) NOT NULL,
-	for_story varchar(36) NOT NULL,
+	by_user int NOT NULL,
+	for_story int NOT NULL,
 	PRIMARY KEY(by_user,for_story),
 	FOREIGN KEY(by_user) REFERENCES user(id),
 	FOREIGN KEY(for_story) REFERENCES story(id),
